5baefba6fe9635596d03fcd9d66dbb8d142d8d38
Justin Fong <justinvf2018@gmail.com>
2025-01-19 18:16:03 -0800

    added player rocket

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
-console.log("Hello World");
+console.log("Hello World")
 
 
 let config = {
     type: Phaser.AUTO,
     width: 640,
-    height: 480
-
+    height: 480,
+    scene:[Menu, Play]
 }
 
-let game = new Phaser.Game(config);
+let game = new Phaser.Game(config)
+
+// set UI sizes
+let borderUISize = game.config.height / 15
+let borderPadding = borderUISize / 3
+
+
+//keyboard Bindings
+let keyFIRE, keyRESET, keyLEFT, keyRIGHT
